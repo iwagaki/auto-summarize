282c8bb4590ea399cc593f8059bbdf492e0f6f63
iwagaki <iwagaki@users.sourceforge.net>
2010-04-08 02:24:33 +0900

    initial commit

---
+++ cron_scraper.rb
@@ -0,0 +1,114 @@
+#!/usr/bin/ruby -Ku
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 iwagaki <iwagaki@users.sourceforge.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# 環境変数
+#   GMAIL_USERNAME
+#   GMAIL_PASSWORD
+#   GMAIL_ADDRESS
+
+require 'rubygems'
+require 'mechanize'
+require 'cgi'
+require 'kconv'
+
+require 'gmail'
+
+$news_array = Array.new
+
+def getNews(page, base_url, max)
+  news = ""
+  flag = false
+  
+#  vlist = {"rnk"=>"総合", "rnk_soc"=>"社会", "rnk_pol"=>"政治", "rnk_eco"=>"経済", "rnk_spo"=>"スポーツ", "rnk_int"=>"国際", "rnk_ind"=>"企業", "rnk_afp"=>"ワールドEYE", "rnk_ent"=>"エンタメ"}
+  vlist = {"rnk_soc"=>"社会", "rnk_pol"=>"政治", "rnk_eco"=>"経済", "rnk_spo"=>"スポーツ", "rnk_int"=>"国際", "rnk_ind"=>"企業", "rnk_afp"=>"ワールドEYE", "rnk_ent"=>"エンタメ"}
+
+
+  page.search('div.ranking-box').each do |box|
+    count = 1
+#    puts entry
+    rnk_name = box.search('a').first['name']
+    if vlist.key?(rnk_name)
+      news << "<h3>#{vlist[rnk_name]}</h3>"
+      box.search('a').each do |entry|
+        if entry['name'] == nil
+          if count > max
+            break
+          end
+          count += 1
+#          title = entry.inner_text
+          url = base_url + entry['href']
+
+          linked_page = getPage(url)
+          title = linked_page.search('title')[1].inner_text #.sub(/時事ドットコム：/, "")
+          news << "<a href=\"#{url}\">#{title}</a><p>\n"
+        end
+      end
+    end
+  end
+
+  return news
+end
+
+def getPage(url)
+  agent = WWW::Mechanize.new
+  page = agent.get(url)
+#  page.body = page.body.toutf8
+  return page
+end
+
+def checkUpdate(page)
+  if page.body =~ /^<dc:date>(.*)<\/dc:date>/
+    return $+
+  end
+  exit
+end
+
+$last_update = ""
+load 'last_update.rb'
+
+mail = ""
+
+page = getPage('http://www.jiji.com/rss/ranking.rdf')
+update_time = checkUpdate(page)
+
+page = getPage('http://www.jiji.com/jc/r')
+
+if ($DEBUG || update_time != $last_update)
+  File.open("last_update.rb", "wb") {|file| file.write('$last_update = "' + update_time + '"' + "\n")}
+  mail << "<html>\n"
+  mail << "<head>\n"
+  mail << "</head>\n"
+  mail << "<body>\n"
+  mail << getNews(page, 'http://www.jiji.com/jc/', 20)
+  mail << "</body>\n"
+  mail << "</html>\n"
+end
+
+plugin_name = "jiji_tsushin"
+
+if mail != ""
+  gmail = Gmail.new(ENV['GMAIL_USERNAME'], ENV['GMAIL_PASSWORD'], ENV['GMAIL_ADDRESS'])
+  gmail.subject = "cron_scraper.rb #{plugin_name} #{update_time}"
+  gmail.message = mail.tosjis.tojis
+  gmail.send_html(ENV['GMAIL_ADDRESS'])
+end
---
+++ gmail.rb
@@ -0,0 +1,85 @@
+#!/usr/bin/ruby -Ku
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 iwagaki <iwagaki@users.sourceforge.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+require 'tlsmail'
+require 'time'
+
+class Gmail
+  attr_accessor :subject
+  attr_accessor :message
+
+  def initialize(gmail_user, gmail_password, gmail_address)
+    @gmail_user = gmail_user
+    @gmail_password = gmail_password
+    @gmail_address = gmail_address
+  end
+
+  def send_html(to_address)
+    @to_address = to_address
+    content = get_plain_header()
+    content << get_html_header()
+    content << "\n"
+    content << @message
+    send(content)
+  end
+
+  def send_plain(to_address)
+    @to_address = to_address
+    content = get_plain_header()
+    content << "\n"
+    content << @message
+    send(content)
+  end
+
+  private
+  def send(content)
+    Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+    Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', @gmail_user, @gmail_password, :login) { |smtp|
+      smtp.send_message(content, @gmail_address, @to_address)
+    }
+  end
+
+  def get_plain_header()
+    content = <<"EOB"
+From: #{@gmail_address}
+To: #{@to_address}
+Subject: #{@subject}
+Date: #{Time.now.rfc2822}
+EOB
+    return content
+  end
+
+  def get_html_header()
+    content = <<"EOB"
+Content-Type: multipart/related; boundary="boundary-here--"
+
+--boundary-here--
+Content-Type: text/html; charset="ISO-2022-JP"
+EOB
+    return content
+  end
+end
+
+# if __FILE__ == $0
+#   Gmail.send("Gmail_account_name", "Gmail_password", "From_address", "To_address", "Subject", "Body")
+# end
